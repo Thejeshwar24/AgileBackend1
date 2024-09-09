d38cb87fee6f2f2837c52141e48e6b1690a63ae4
Thejeshwar <thejeshwarsy@gmail.com>
2024-09-09 14:59:22 +0530

    tasks-api

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,11 +2,10 @@ import express from 'express';
 import { createTask, getTasks,getTaskById,updateTask,deleteTask,getTasksByUserName } from '../controllers/taskController.js';
 
 const router = express.Router();
+router.get('/byUserName', getTasksByUserName);
 
 router.post('/', createTask);
 router.get('/', getTasks);
-//
-router.get('/tasks/byUserName', getTasksByUserName);
 router.get('/:id', getTaskById);
 router.put('/:id', updateTask);
 router.delete('/:id', deleteTask);
